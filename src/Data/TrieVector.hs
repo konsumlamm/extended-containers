b2e9f141b2c3be0189bd8fe60e16a9b80c90b44f
konsumlamm <konsumlamm@gmail.com>
2019-09-08 04:06:39 +0200

    use Data.List.NonEmpty for tail, add !? operator

--- src/Data/TrieVector.hs
+++ src/Data/TrieVector.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Data.TrieVector
 ( Vector
 , empty
@@ -5,6 +7,7 @@ module Data.TrieVector
 , fromList
 , snoc
 , lookup
+, (!?)
 , update
 , adjust
 , map
@@ -12,20 +15,22 @@ module Data.TrieVector
 
 import Data.Bits
 import Data.Foldable (foldl', toList)
-import Data.Vector((!))
+import Data.List.NonEmpty (NonEmpty(..), (!!), (<|))
+import qualified Data.List.NonEmpty as L
+import Data.Vector ((!))
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as M
-import Prelude hiding (lookup, map, tail)
+import Prelude hiding ((!!), lookup, map, tail)
 
 -- An Array Mapped Trie.
 data Tree a = Internal !(V.Vector (Tree a))
             | Leaf !(V.Vector a)
 data Vector a = Empty
-              | Root !Int  -- ^ size
-                     !Int  -- ^ offset
-                     !Int  -- ^ height
-                     !(Tree a)  -- ^ tree
-                     ![a]  -- ^ tail (reversed)
+              | Root !Int  -- size
+                     !Int  -- offset
+                     !Int  -- height
+                     !(Tree a)  -- tree
+                     !(NonEmpty a)  -- tail (reversed)
 
 -- | The number of bits used per level.
 bits :: Int
@@ -35,7 +40,6 @@ bits = 4
 -- | The mask used to extract the index into the array.
 mask :: Int
 mask = (1 `shiftL` bits) - 1
-{-# INLINE mask #-}
 
 
 instance Show a => Show (Vector a) where
@@ -51,7 +55,7 @@ instance Monoid (Vector a) where
 
 instance Foldable Vector where
     foldr _ acc Empty = acc
-    foldr f acc (Root _ _ _ tree tail) = foldrTree tree (foldr f acc (reverse tail))
+    foldr f acc (Root _ _ _ tree tail) = foldrTree tree (foldr f acc (L.reverse tail))
       where
         foldrTree (Internal v) acc = foldr foldrTree acc v
         foldrTree (Leaf v) acc = foldr f acc v
@@ -65,10 +69,11 @@ instance Functor Vector where
 
 instance Traversable Vector where
     traverse _ Empty = pure Empty
-    traverse f (Root s offset h tree tail) = Root s offset h <$> traverseTree tree <*> (reverse <$> traverse f (reverse tail))
+    traverse f (Root s offset h tree tail) = Root s offset h <$> traverseTree tree <*> (L.reverse <$> traverse f (L.reverse tail))
       where
         traverseTree (Internal v) = Internal <$> traverse traverseTree v
         traverseTree (Leaf v) = Leaf <$> traverse f v
+
 
 -- | /O(1)/. The empty vector.
 empty :: Vector a
@@ -85,12 +90,12 @@ fromList = foldl' snoc empty
 snoc :: Vector a -> a -> Vector a
 snoc Empty x = singleton x
 snoc (Root s offset h tree tail) x
-    | s .&. mask /= 0 = Root (s + 1) offset h tree (x : tail)
-    | offset == 0 = Root (s + 1) s (h + 1) (Leaf $ V.fromList (reverse tail)) [x]
+    | s .&. mask /= 0 = Root (s + 1) offset h tree (x <| tail)
+    | offset == 0 = Root (s + 1) s (h + 1) (Leaf $ V.fromList (toList $ L.reverse tail)) [x]
     | offset == 1 `shiftL` (bits * h) = Root (s + 1) s (h + 1) (Internal $ V.fromList [tree, newPath h]) [x]
     | otherwise = Root (s + 1) s h (insertTail (bits * (h - 1)) tree) [x]
   where
-    newPath 1 = Leaf $ V.fromList (reverse tail)
+    newPath 1 = Leaf $ V.fromList (toList $ L.reverse tail)
     newPath h = Internal $ V.singleton (newPath (h - 1))
 
     insertTail sh (Internal v)
@@ -98,8 +103,25 @@ snoc (Root s offset h tree tail) x
         | otherwise = Internal $ V.snoc v (newPath (sh `div` bits))
       where
         index = offset `shiftR` sh .&. mask
-    insertTail _ (Leaf _) = Leaf $ V.fromList (reverse tail)
+    insertTail _ (Leaf _) = Leaf $ V.fromList (toList $ L.reverse tail)
+{-
+unsnoc :: Vector a -> Maybe (Vector a, a)
+unsnoc Empty = Nothing
+unsnoc (Root s offset h tree (x :| tail))
+    | s == 1 = Just (Empty, x)
+    | (s - 1) .&. mask /= 0 = Just (Root (s - 1) offset h tree (L.fromList tail), x)
+    | () =
+        let Internal v = tree
+        in Just (Root (s - 1) ??? (h - 1) (V.head v) (L.fromList $ toList (lastLeaf tree)), x)
+    | otherwise = Just (Root (s - 1) ??? h (removeTail tree) (L.fromList $ toList (lastLeaf tree)), x)
+  where
+    lastLeaf (Internal v) = lastLeaf (V.last v)
+    lastLeaf (Leaf v) = Leaf v
 
+    removeTail (Internal v)
+        | V.
+    removeTail (Leaf _) = Leaf V.empty
+-}
 lookup :: Int -> Vector a -> Maybe a
 lookup _ Empty = Nothing
 lookup i (Root s offset h tree tail)
@@ -110,6 +132,9 @@ lookup i (Root s offset h tree tail)
     lookupTree sh (Internal v) = lookupTree (sh - bits) (v ! (i `shiftR` sh .&. mask))
     lookupTree _ (Leaf v) = v ! (i .&. mask)
 
+(!?) :: Vector a -> Int -> Maybe a
+(!?) = flip lookup
+
 update :: Int -> a -> Vector a -> Vector a
 update i x = adjust i (const x)
 {-# INLINE update #-}
@@ -119,7 +144,7 @@ adjust _ _ Empty = Empty
 adjust i f root@(Root s offset h tree tail)
     | i < 0 || i >= s = root
     | i < offset = Root s offset h (adjustTree (bits * (h - 1)) tree) tail
-    | otherwise = let (l, x : r) = splitAt (s - i - 1) tail in Root s offset h tree (l ++ (f x : r))
+    | otherwise = let (l, x : r) = L.splitAt (s - i - 1) tail in Root s offset h tree (L.fromList $ l ++ (f x : r))
   where
     adjustTree sh (Internal v) =
         let index = i `shiftR` sh .&. mask
