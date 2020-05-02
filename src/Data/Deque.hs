40b2de60ee6b055551498b01f7674156589d7ea3
konsumlamm <konsumlamm@gmail.com>
2020-05-02 12:12:43 +0200

    Add more Deque instances
    Add Show1, Show, Read1, Read, Traversable, IsList instances for Deque

--- src/Data/Deque.hs
+++ src/Data/Deque.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE CPP #-}
+#ifdef __GLASGOW_HASKELL__
+{-# LANGUAGE TypeFamilies #-}
+#endif
+
 {- TODO:
     zip, zipWith, zip3, zipWith3, unzip, unzip3?
-    fromFunction, replicate, replicateA, unfoldr, unfoldl, iterateN?
+    fromFunction, replicate, replicateA, iterateN?
     take, drop, splitAt
     takeWhileL, takeWhileR, dropWhileL, dropWhileR, spanl, spanr, breakl, breakr
     partition, filter, append/concat?
@@ -51,7 +56,12 @@ module Data.Deque
 import Data.Foldable (toList)
 import Data.Functor.Classes
 import qualified Data.List as List
+#ifdef __GLASGOW_HASKELL__
+import GHC.Exts (IsList)
+import qualified GHC.Exts as Exts
+#endif
 import Prelude hiding (map, reverse, tail)
+import Text.Read (Lexeme(Ident), lexP, parens, prec, readPrec)
 
 infixr 5 <|
 infixl 5 |>
@@ -61,7 +71,7 @@ tail [] = []
 tail (_ : xs) = xs
 
 -- | Invariant: @length ls <= 3 * length rs + 1 && length rs <= 3 * length ls + 1@.
-data Deque a = Deque !Int [a] [a] !Int [a] [a] deriving Show
+data Deque a = Deque !Int [a] [a] !Int [a] [a]
 
 check :: Deque a -> Deque a
 check d@(Deque ll ls _ rl rs _)
@@ -86,25 +96,65 @@ check d@(Deque ll ls _ rl rs _)
     rot2 [] rs xs = List.reverse rs ++ xs
     rot2 (l : ls) rs xs = l : rot2 ls (drop 3 rs) (List.reverse (take 3 rs) ++ xs)
 
+instance Show1 Deque where
+    liftShowsPrec sp sl p v = showsUnaryWith (liftShowsPrec sp sl) "fromList" p (toList v)
+
+instance Show a => Show (Deque a) where
+    showsPrec = showsPrec1
+    {-# INLINE showsPrec #-}
+
+instance Read1 Deque where
+    liftReadsPrec rp rl = readsData $ readsUnaryWith (liftReadsPrec rp rl) "fromList" fromList
+
+instance Read a => Read (Deque a) where
+#ifdef __GLASGOW_HASKELL__
+    readPrec = parens $ prec 10 $ do
+        Ident "fromList" <- lexP
+        xs <- readPrec
+        pure (fromList xs)
+#else
+    readsPrec = readsPrec1
+    {-# INLINE readsPrec #-}
+#endif
+
 instance Functor Deque where
     fmap = map
     {-# INLINE fmap #-}
 
 instance Foldable Deque where
-    -- TODO: use viewl instead?
     foldr f acc (Deque _ ls _ _ rs _) = foldr f (foldl (flip f) acc rs) ls
 
     length (Deque ll _ _ rl _ _) = ll + rl
+
+instance Traversable Deque where
+    traverse f = go
+      where
+        go d = case viewl d of
+            Nothing -> pure empty
+            Just (x, d') -> (<|) <$> f x <*> go d'
+    {-# INLINE traverse #-}
+
 {-
 instance Eq a => Eq (Deque a) where
     (==) = eq1
     {-# INLINE (==) #-}
 -}
+
+#ifdef __GLASGOW_HASKELL__
+instance IsList (Deque a) where
+    type Item (Deque a) = a
+
+    fromList = fromList
+    {-# INLINE fromList #-}
+
+    toList = toList
+    {-# INLINE toList #-}
+#endif
+
 -- TODO: instances
 -- Show1, Show, Read1, Read, Eq1, Ord1, Ord
 -- Semigroup, Monoid
--- Trversable, Applicative, Alternative, Monad, MonadPlus, MonadFail, MonadZip
--- IsList
+-- Applicative, Alternative, Monad, MonadPlus, MonadFail, MonadZip
 
 
 -- | /O(1)/.
@@ -120,6 +170,10 @@ singleton x = Deque 1 [x] [] 0 [] []
 fromList :: [a] -> Deque a
 fromList = foldr (<|) empty
 {-# INLINE fromList #-}
+
+-- TODO: unfoldr :: (b -> Maybe (a, b)) -> b -> Deque a
+
+-- TODO: unfoldl :: (b -> Maybe (b, a)) -> b -> Deque a
 
 -- | /O(1)/.
 (<|) :: a -> Deque a -> Deque a
@@ -145,7 +199,7 @@ viewr (Deque ll ls ls' rl (x : rs) rs') = Just (check $ Deque ll ls (tail (tail 
 reverse :: Deque a -> Deque a
 reverse (Deque ll ls ls' rl rs rs') = Deque rl rs rs' ll ls ls'
 
--- TODO: use viewl + (<|)?
 -- | /O(n)/.
+-- TODO
 map :: (a -> b) -> Deque a -> Deque b
 map f (Deque ll ls ls' rl rs rs') = Deque ll (fmap f ls) (fmap f ls') rl (fmap f rs) (fmap f rs')
