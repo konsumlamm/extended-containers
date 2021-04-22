04acf2eb83527cc22650574cd0660b73053a6afa
konsumlamm <konsumlamm@gmail.com>
2021-04-22 16:51:23 +0200

    Use primitive instead of vector
    Update to cabal >= 2.0

--- src/Util/Internal/Array.hs
+++ src/Util/Internal/Array.hs
@@ -14,56 +14,63 @@ module Util.Internal.Array
     , toTail
     ) where
 
--- TODO: use SmallArray or similar
-
-import Data.List.NonEmpty as L hiding (head, last, take)
+import Data.List.NonEmpty as L hiding (head, last, take, length)
 import Prelude hiding (head, last, take)
 
-import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as M
+import Data.Primitive.SmallArray
 
-type Array a = V.Vector a
+type Array a = SmallArray a
 
 empty :: Array a
-empty = V.empty
+empty = mempty
 
 singleton :: a -> Array a
-singleton = V.singleton
+singleton x = runSmallArray $ newSmallArray 1 x
 
 snoc :: Array a -> a -> Array a
-snoc = V.snoc
+snoc arr x = runSmallArray $ do
+    let size = length arr
+    arr' <- newSmallArray (size + 1) x
+    copySmallArray arr' 0 arr 0 size
+    pure arr'
 
 index :: Int -> Array a -> a
-index = flip V.unsafeIndex
+index = flip indexSmallArray
 
 head :: Array a -> a
-head = V.unsafeHead
+head arr = indexSmallArray arr 0
 
 last :: Array a -> a
-last = V.unsafeLast
+last arr = indexSmallArray arr (length arr - 1)
 
 -- | Update the element at the specified index.
 adjust :: Int -> (a -> a) -> Array a -> Array a
-adjust i f = V.modify (\v -> M.unsafeModify v f i)
+adjust i f arr = runSmallArray $ do
+    arr' <- thawSmallArray arr 0 (length arr)
+    let x = indexSmallArray arr i
+    writeSmallArray arr' i (f x)
+    pure arr'
 {-# INLINE adjust #-}
 
 take :: Int -> Array a -> Array a
-take = V.unsafeTake
+take n arr = cloneSmallArray arr 0 n
 
 fromList2 :: a -> a -> Array a
-fromList2 x y = V.fromListN 2 [x, y]
+fromList2 x y = runSmallArray $ do
+    arr <- newSmallArray 2 x
+    writeSmallArray arr 1 y
+    pure arr
 
 -- | Convert a full tail into an array.
 --
 -- > fromTail = A.fromListN tailSize . reverse . toList
 fromTail :: Int -> L.NonEmpty a -> Array a
-fromTail size (x :| xs) = V.create $ do
-    v <- M.new size
-    M.unsafeWrite v (size - 1) x  -- mask = tailSize - 1
+fromTail size (x :| xs) = runSmallArray $ do
+    arr <- newSmallArray size x
     let loop _ [] = pure ()
-        loop i (y : ys) = M.unsafeWrite v i y *> loop (i - 1) ys
+        loop i (y : ys) = writeSmallArray arr i y *> loop (i - 1) ys
     loop (size - 2) xs
-    pure v
+    pure arr
 
 -- | Convert an array into a tail.
 --
